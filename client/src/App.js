abe1a25a324c6de7444e5bf52621b7a8c40549eb
Lisa <lb.broadhead@gmail.com>
2022-06-06 12:40:24 -0500

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 import {BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 import Index from './components/Index';
+import Blog from './components/Blog';
 
 function App() {
   return (
     <BrowserRouter >
+
+      <Switch>
+        <Route exact path="/">
           <Index />
+        </Route>
+
+        <Route exact path="/blog">
+          <Blog />
+        </Route>
+      </Switch>
+
     </BrowserRouter>
   );
 }
--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import lisa from '../img/lisa_broadhead.jpeg'
 
 const Blog = (props) => {
     return(
-        <div className="blogWrp">
-            <h1>blog</h1>
-        </div>
+        <>
+            <div class="banner">
+            <h1>Blog</h1>
+            <p className="subtitle">"I am still learning." <span className='author'>- Michelangelo</span></p>
+            </div>
+            
+            <div className="container">
+                <main>
+                    <div className='mostRecten'>
+                        {/* Most recent list of blog posts */}
+                    </div>
+                </main>
+
+                <aside>
+                    <img src={lisa} alt="Lisa Broadhead - Software Engineer | Data Scientist Machine Learnering Engineer" />
+                    
+                    <p className="paragraph">I'm always learning and therfore always failing (it sucks, but everything has a price). I began this project as a way to keep track of my htoughts and notes. I use to use Google Docs, which worked great, but it wasn't visuale or interactive enought for me.</p>
+                    <div className='socialWrp'>
+
+                    </div>
+
+                    <ul className='categories'>
+                        {/* Cageories from the database */}
+                    </ul>
+                </aside>
+                
+            </div>
+
+        </>
     )
 }
 
