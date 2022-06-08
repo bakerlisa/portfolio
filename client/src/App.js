5576e6450ee79789ce065061305a01c13c572083
Lisa <lb.broadhead@gmail.com>
2022-06-08 08:31:04 -0500

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+import { Switch,Route,Link,Redirect, useHistory, BrowserRouter } from 'react-router-dom';
 import './App.scss';
 import axios from 'axios'
 import React, {useState, useEffect} from 'react'
 
 import Index from '../../client/src/components/Index';
 import Blog from '../../client/src/components/Blog';
+import AddBlog from '../../client/src/components/AddBlog';
+import EditBlog from '../../client/src/components/EditBlog';
 
 function App() {
   const [data,setData] = useState([{}])
@@ -16,9 +19,34 @@ function App() {
   }, [])
 
   return (
-    <div>
-      <h1>React + Python here we come!</h1>
-    </div>
+    
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Index />
+          </Route>
+        </Switch>
+
+        <Switch>
+          <Route exact path="/blog">
+            <Blog />
+          </Route>
+        </Switch>
+
+        <Switch>
+          <Route exact path="/edit-blog">
+            <EditBlog />
+          </Route>
+        </Switch>
+
+        <Switch>
+          <Route exact path="/add-blog">
+            <AddBlog />
+          </Route>
+        </Switch>
+
+      </BrowserRouter>
+
   );
 }
 
