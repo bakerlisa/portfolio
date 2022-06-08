805931d7910b65ec894e3769bba7fbb1a8e73701
Lisa <lb.broadhead@gmail.com>
2022-06-08 07:44:48 -0500

    first commit

--- client/src/components/AddBlog.js
+++ client/src/components/AddBlog.js
@@ -3,6 +3,25 @@ import React from 'react';
 const AddBlog = () => {
     return(
         <>
+        
+        <form action="">
+            <div>
+                <label htmlFor="tag">Tag: <sapn>Comma seperated list</sapn></label>
+                <input type="text" name="tag" />
+            </div>
+
+            <div>
+                <label htmlFor="title">Title</label>
+                <input type="text" name="title" />
+            </div>
+
+            <div>
+                <label htmlFor="tag">Content:</label>
+                <textarea type="text" name="content" >
+                
+                </textarea>
+            </div>
+        </form>
         </>
     )
 }
