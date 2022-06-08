6dcb3fff58368cbd7b66cd5583a3ea7ae734b3b9
Lisa <lb.broadhead@gmail.com>
2022-06-08 07:37:01 -0500

    first commit

---
+++ client/src/components/AddBlog.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AddBlog = () => {
+    return(
+        <>
+        </>
+    )
+}
+
+export default AddBlog;
---
+++ client/src/components/EditBlog.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const EditBlog = () => {
+    return(
+        <>
+        </>
+    )
+}
+
+export default EditBlog;
