c7219f535d8f89afc448f4752d752f4574d593b3
Lisa <lb.broadhead@gmail.com>
2022-06-08 08:18:06 -0500

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import './App.scss';
 import axios from 'axios'
 import React, {useState, useEffect} from 'react'
 
-import Index from './components/Index';
-import Blog from './components/Blog';
+import Index from '../../client/src/components/Index';
+import Blog from '../../client/src/components/Blog';
 
 function App() {
   const [data,setData] = useState([{}])
