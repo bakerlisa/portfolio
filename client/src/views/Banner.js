1af8adad5f211e6939fe5ca211aa0a9692549dac
Lisa <lb.broadhead@gmail.com>
2022-06-09 07:43:42 -0500

    first commit

--- client/src/views/Banner.js
+++ client/src/views/Banner.js
@@ -6,7 +6,7 @@ import React from 'react'
 const Banner = (props) => {
     return(
         
-        <div className="banner" style={{backgroundImage: `url(${process.env.PUBLIC_URL}/${props.img})`}} >
+        <div className="banner" style={{backgroundImage: `url(http://localhost:3000/client/src//img/${props.img})`}} >
             <div className="txt-wrp">
                 <h1 className="title">{props.title}</h1>
                 <h4>{props.subtitle}</h4>
